491383c8f62dc63be62ee526ba5df7a3a3607b01
Che Tranqui <chaz.peterson1@gmail.com>
2021-03-08 11:42:04 -0600

    Contact Me update
    added copy-to-clipboard functionality, with a notification, to the Contact Me link

--- mainPage.js
+++ mainPage.js
@@ -20,10 +20,43 @@ window.onclick = function (event) {
 //carousel side buttons get centered to the image and match height on load and resize
 window.onresize = resizeButtons;
 
-document.addEventListener('DOMContentLoaded', resizeButtons());
+document.addEventListener('DOMContentLoaded', () => {
+    resizeButtons();
+    $("emailMeLink").addEventListener("click", copyEmail);
+});
 
 function resizeButtons() {
     let idealButtonHeight = $("projectImage").height;
     $("carouselRightButton").style.height = idealButtonHeight + "px";
     $("carouselLeftButton").style.height = idealButtonHeight + "px";
+}
+
+function copyEmail() {
+    //confirm permission to write to clipboard
+    navigator.permissions.query({ name: "clipboard-write" }).then(result => {
+        //if so copy email address and let user know
+        if (result.state == "granted" || result.state == "prompt") {
+            navigator.clipboard.writeText("chetranqui@gmail.com").then(function () {
+                displayCopyPrompt();
+            }).catch(function() {
+                //otherwise do nothing
+            });
+        }
+    })
+}
+
+function displayCopyPrompt() {
+    //get width of navBar - use that width to center notification
+    let leftOffset = ($("navContainer").offsetWidth / 2);
+    $("copyNotification").style.left = "" + leftOffset + "px";
+    $("copyNotification").style.transform = "translateX(-40%)";
+    $("copyNotification").style.display = "inline";
+    hideElement($("copyNotification"), 3000);
+}
+
+// wait before hiding results screen
+function hideElement(element, ms) {
+    setTimeout(function () {
+        element.style.display = "none";
+    }, ms);
 }

fc779029349e5ad958a4bb881ae0bdf8ff791af9
CheTranqui <CheTranqui@users.noreply.github.com>
2021-07-03 23:48:39 -0500

    DRY refactor for s2 and s4
    pulled out assign/splice loop for squareTwo and squareFour for DRY purposes - improved code legibility and consistency

--- JSSudoku/sudoku.js
+++ JSSudoku/sudoku.js
@@ -38,7 +38,6 @@ function clearBoard() {
             board[i][j] = 0;
         }
     }
-    filled = [];
 }
 
 function generateBoard() {
@@ -151,92 +150,81 @@ function determineFirstSquare() {
     return squareOne;
 }
 
+function assignDestinationValues(loopConstraint, source, destination, possibilities){
+    // loop constraint = how many spaces need filled - determined by either:
+    // available numbers in source or available space in destination
+    // source = originating array of up to 3 numbers
+    // destination = array that numbers are being assigned into
+    // possibilities = array listing unassigned indexes in destination that still may be filled
+    for (i = 0; i < loopConstraint; i++){
+        let possibilitiesIndex = getRandom(possibilities.length);
+        let sourceIndex = getRandom(source.length);
+        // set appropriate destination index equal to source index
+        destination[possibilities[possibilitiesIndex]] = source[sourceIndex];
+        // splice index out of possibilities and from source to keep them from getting reused
+        possibilities.splice(possibilitiesIndex,1);
+        source.splice(sourceIndex, 1);
+    }
+    return [source, destination, possibilities];
+}
+
 function determineSecondSquare(squareOne){
     // s2 middle row = get 1 or 2 from s1 top:
     // SQUARE STATUS:  empty
-    const totalTopNumbersForS2Middle = getRandom(4); //getRandom is based on getting an index from a length - 4 can never occur, 3 is max here
+    //get returns a number from 0-3 (this is how many from s1Top will be put in s2Middle)
+    const totalTopNumbersForS2Middle = getRandom(4);
     // s1Top only contains values that need placed in s2Middle or s2Bottom
-    let s1Top = [squareOne[0], squareOne[1], squareOne[2]];
-    let s2Middle = [0,0,0];
-    // whatever value is left in s2Possibilities is an unfilled index in s2Middle
-    let s2MiddlePossibilities = [0,1,2]
-    for (i = 0; i < totalTopNumbersForS2Middle; i++){
-        let randomPossibilitiesIndex = getRandom(s2MiddlePossibilities.length); // get random index of options
-        let randomS1TopIndex = getRandom(s1Top.length); // get random index from top
-        // set appropriate s2Middle value to s1value
-        s2Middle[s2MiddlePossibilities[randomPossibilitiesIndex]] = s1Top[randomS1TopIndex];
-        // splice index out of possibile s2Middle indexes and out of s1Top numbers
-        s2MiddlePossibilities.splice(randomPossibilitiesIndex,1);
-        s1Top.splice(randomS1TopIndex, 1)
-    }
+    let s1Top = [squareOne[0], squareOne[1], squareOne[2]];      // the two arrays indicate a new, empty destination and all possibilities
+    let results = assignDestinationValues(totalTopNumbersForS2Middle, s1Top, [0,0,0], [0,1,2]);
+    s1Top = results[0];
+    let s2Middle = results[1];
+    let s2MiddlePossibilities = results[2];
+    // whatever values are left in s2Possibilities is an unfilled index in s2Middle
+    
     // SQUARE STATUS:  top empty, middle partial, bottom empty
 
-    let s2Bottom = [0,0,0];
-    let s2BottomPossibilities = [0,1,2];
-    const s1TopLength = s1Top.length;
-    for (i = 0; i < s1TopLength; i++){
-        // toss remaining top numbers into random bottom index
-        let randomS1TopIndex = getRandom(s1Top.length);
-        let randomPossibilitiesIndex = getRandom(s2BottomPossibilities.length);
-        s2Bottom[s2BottomPossibilities[randomPossibilitiesIndex]] = s1Top[randomS1TopIndex];
-        s2BottomPossibilities.splice(randomPossibilitiesIndex, 1);
-        s1Top.splice(randomS1TopIndex, 1);
-    }
+    // assign remaining values in s1Top to s2Bottom      the two arrays indicate a new, empty destination and all possibilities
+    results = assignDestinationValues(s1Top.length, s1Top, [0,0,0], [0,1,2]);
+    s1Top = results[0]; // should be empty array
+    let s2Bottom = results[1];
+    let s2BottomPossibilities = results[2];
+    // s1Top is empty
 
     // SQUARE STATUS:  top empty, middle partial, bottom partial
 
-    // fill remaining spaces in Middle with random indexes from s1 bottom
-    let s1Bottom = [squareOne[6], squareOne[7], squareOne[8]]
-    const s2MiddleLength = s2MiddlePossibilities.length;
-    for (i = 0; i < s2MiddleLength; i++){
-        let randomS1BottomIndex = getRandom(s1Bottom.length);
-        let randomPossibilitiesIndex = getRandom(s2MiddlePossibilities.length);
-        s2Middle[s2MiddlePossibilities[randomPossibilitiesIndex]] = s1Bottom[randomS1BottomIndex];
-        s2MiddlePossibilities.splice(randomPossibilitiesIndex, 1);
-        s1Bottom.splice(randomS1BottomIndex, 1);
-    }
+    // fill remaining spaces in Middle with s1 bottom
+    let s1Bottom = [squareOne[6], squareOne[7], squareOne[8]];
+    results = assignDestinationValues(s2MiddlePossibilities.length, s1Bottom, s2Middle, s2MiddlePossibilities);
+    s1Bottom = results[0];
+    s2Middle = results[1]; // should be full (array length 3)
+    s2MiddlePossibilities = results[2];  // should be an empty array now
 
     // SQUARE STATUS:  top empty, middle full, bottom partial
 
-    // any remaining s1Bottom must go into s2Top.
-    let s2Top = [0,0,0];
-    let s2TopPossibilities = [0,1,2];
-    const s1BottomLength = s1Bottom.length;
-    for (i = 0; i < s1BottomLength; i++){
-        randomS1BottomIndex = getRandom(s1Bottom.length);
-        randomPossibilitiesIndex = getRandom(s2TopPossibilities.length);
-        s2Top[s2TopPossibilities[randomPossibilitiesIndex]] = s1Bottom[randomS1BottomIndex];
-        s2TopPossibilities.splice(randomPossibilitiesIndex, 1);
-        s1Bottom.splice(randomS1BottomIndex, 1);
-    }
+    // any remaining s1Bottom must go into s2Top.      the two arrays indicate a new, empty destination and all possibilities
+    results = assignDestinationValues(s1Bottom.length, s1Bottom, [0,0,0], [0,1,2]);
+    s1Bottom = results[0]; // should be empty
+    let s2Top = results[1];
+    let s2TopPossibilities = results[2];
 
     // SQUARE STATUS:  top partial, middle full, bottom partial
 
     // fill in remaining s2Top from s1Middle
     let s1Middle = [squareOne[3], squareOne[4], squareOne[5]];
-    const s2TopLength = s2TopPossibilities.length;
-    for (i = 0; i < s2TopLength; i++){
-        let randomS1MiddleIndex = getRandom(s1Middle.length);
-        let randomPossibilitiesIndex = getRandom(s2TopPossibilities.length);
-        s2Top[s2TopPossibilities[randomPossibilitiesIndex]] = s1Middle[randomS1MiddleIndex];
-        s2TopPossibilities.splice(randomPossibilitiesIndex, 1);
-        s1Middle.splice(randomS1MiddleIndex, 1);
-    }
+    results = assignDestinationValues(s2TopPossibilities.length, s1Middle, s2Top, s2TopPossibilities);
+    s1Middle = results[0];
+    s2Top = results[1]; // should be full (array length 3)
+    s2TopPossibilities = results[2]; // should be empty
 
     // SQUARE STATUS:  top full, middle full, bottom partial
 
     // fill in remaining s2Bottom from s1Middle
-    const s2BottomLength = s2BottomPossibilities.length;
-    for (i = 0; i < s2BottomLength; i++){
-        let randomS1MiddleIndex = getRandom(s1Middle.length);
-        let randomPossibilitiesIndex = getRandom(s2BottomPossibilities.length);
-        s2Bottom[s2BottomPossibilities[randomPossibilitiesIndex]] = s1Middle[randomS1MiddleIndex];
-        s2BottomPossibilities.splice(randomPossibilitiesIndex, 1);
-        s1Middle.splice(randomS1MiddleIndex, 1);
-    }
+    results = assignDestinationValues(s2BottomPossibilities.length, s1Middle, s2Bottom, s2BottomPossibilities);
+    s1Middle = results[0]; // should be empty
+    s2Bottom = results[1]; // should be full
+    s2BottomPossibilities = results[2]; // should be empty
 
-    // SQUARE STATUS:  top full, middle full, bottom full
-
+    // squareTwo results:
     return [s2Top[0], s2Top[1], s2Top[2],
             s2Middle[0], s2Middle[1], s2Middle[2],
             s2Bottom[0], s2Bottom[1], s2Bottom[2]];
@@ -251,7 +239,7 @@ function determineThirdSquare(){
     // return array as third square
     return [ending1[0], ending1[1], ending1[2],
             ending2[0], ending2[1], ending2[2],
-            ending3[0], ending3[1], ending3[2],]
+            ending3[0], ending3[1], ending3[2],];
 }
 
 // removes numbers already present in the row from possibilities
@@ -277,74 +265,50 @@ function determineSquareFour(squareOne){
     const totalLeftNumbersForS4Center = getRandom(4); //getRandom is max-exclusive
     // s1Left only contains values that need placed in s4Center or s2Right
     let s1Left = [squareOne[0], squareOne[3], squareOne[6]];
-    let s4Center = [0,0,0];
-    // whatever value is left in s2Possibilities is an unfilled index in s2Middle
-    let s4CenterPossibilities = [0,1,2];
-    for (i = 0; i < totalLeftNumbersForS4Center; i++){
-        let randomPossibilitiesIndex = getRandom(s4CenterPossibilities.length); // get random index of options
-        let randomS1LeftIndex = getRandom(s1Left.length); // get random index from top
-        // set appropriate s4Center value to s1value
-        s4Center[s4CenterPossibilities[randomPossibilitiesIndex]] = s1Left[randomS1LeftIndex];
-        // splice index out of possibile s2Center indexes and out of s1Left numbers
-        s4CenterPossibilities.splice(randomPossibilitiesIndex,1);
-        s1Left.splice(randomS1LeftIndex, 1)
-    }
+    let results = assignDestinationValues(totalLeftNumbersForS4Center, s1Left, [0,0,0], [0,1,2]);
+    s1Left = results[0];
+    let s4Center = results[1];
+    let s4CenterPossibilities = results[2];
+
     // set remaining s1Left values into s4Right
-    let s4Right = [0,0,0];
-    let s4RightPossibilities = [0,1,2];
-    const s1LeftLength = s1Left.length;
-    for (i = 0; i < s1LeftLength; i++){
-        let randomPossibilitiesIndex = getRandom(s4RightPossibilities.length);
-        let randomS1LeftIndex = getRandom(s1Left.length);
-        s4Right[s4RightPossibilities[randomPossibilitiesIndex]] = s1Left[randomS1LeftIndex];
-        s4RightPossibilities.splice(randomPossibilitiesIndex, 1);
-        s1Left.splice(randomS1LeftIndex, 1);
-    }
+    results = assignDestinationValues(s1Left.length, s1Left, [0,0,0], [0,1,2]);
+    s1Left = results[0]; // should be empty
+    let s4Right = results[1];
+    let s4RightPossibilities = results[2];
+    
     // SQUARE STATUS:  s4Left is empty, s4Center is partial, s4Right is partial
     
     // finish filling s4Right using S1Center
     let s1Center = [squareOne[1], squareOne[4], squareOne[7]];
-    const s4RightPossibilitiesLength = s4RightPossibilities.length;
-    for (i = 0; i < s4RightPossibilitiesLength; i++){
-        let randomPossibilitiesIndex = getRandom(s4RightPossibilities.length);
-        let randomS1CenterIndex = getRandom(s1Center.length);
-        s4Right[s4RightPossibilities[randomPossibilitiesIndex]] = s1Center[randomS1CenterIndex];
-        s4RightPossibilities.splice(randomPossibilitiesIndex, 1);
-        s1Center.splice(randomS1CenterIndex, 1);
-    }
+    results = assignDestinationValues(s4RightPossibilities.length, s1Center, s4Right, s4RightPossibilities);
+    s1Center = results[0];
+    s4Right = results[1]; // should be full (array length 3)
+    s4RightPossibilities = results[2]; // should be empty
+    
     // set remaining numbers from s1Center into s4Left
-    let s4Left = [0,0,0];
-    let s4LeftPossibilities = [0,1,2];
-    const s1CenterLength = s1Center.length;
-    for (i = 0; i < s1CenterLength; i++){
-        let randomPossibilitiesIndex = getRandom(s4LeftPossibilities.length);
-        let randomS1CenterIndex = getRandom(s1Center.length);
-        s4Left[s4LeftPossibilities[randomPossibilitiesIndex]] = s1Center[randomS1CenterIndex];
-        s4LeftPossibilities.splice(randomPossibilitiesIndex, 1);
-        s1Center.splice(randomS1CenterIndex, 1);
-    }
+    results = assignDestinationValues(s1Center.length, s1Center, [0,0,0], [0,1,2]);
+    s1Center = results[0]; // should be empty
+    let s4Left = results[1];
+    let s4LeftPossibilities = results[2];
+
     // SQUARE STATUS:  s4Left is partial, s4Center is partial, s4Right is full
     //                 s1Left is empty, s1Center is empty, s1Right is full
+
     // fill remaining s4Center from s1Right
     let s1Right = [squareOne[2], squareOne[5], squareOne[8]];
-    const s4CenterPossibilitiesLength = s4CenterPossibilities.length;
-    for (i = 0; i < s4CenterPossibilitiesLength; i++){
-        let randomPossibilitiesIndex = getRandom(s4CenterPossibilities.length);
-        let randomS1RightIndex = getRandom(s1Right.length);
-        s4Center[s4CenterPossibilities[randomPossibilitiesIndex]] = s1Right[randomS1RightIndex];
-        s4CenterPossibilities.splice(randomPossibilitiesIndex, 1);
-        s1Right.splice(randomS1RightIndex, 1);
-    }
+    results = assignDestinationValues(s4CenterPossibilities.length, s1Right, s4Center, s4CenterPossibilities);
+    s1Right = results[0];
+    s4Center = results[1]; // should be full (array length 3)
+    s4CenterPossibilities = results[2]; // should be empty
+
     // set remaining numbers from s1Right into s4Left
     // this will fill all remaining squares for squareFour
-    const s4LeftPossibilitiesLength = s4LeftPossibilities.length;
-    for (i = 0; i < s4LeftPossibilitiesLength; i++){
-        let randomPossibilitiesIndex = getRandom(s4LeftPossibilities.length);
-        let randomS1RightIndex = getRandom(s1Right.length);
-        s4Left[s4LeftPossibilities[randomPossibilitiesIndex]] = s1Right[randomS1RightIndex];
-        s4LeftPossibilities.splice(randomPossibilitiesIndex, 1);
-        s1Right.splice(randomS1RightIndex, 1);
-    }
+    results = assignDestinationValues(s4LeftPossibilities.length, s1Right, s4Left, s4LeftPossibilities);
+    s1Right = results[0]; // should be empty
+    s4Left = results[1]; // should be full (array length 3)
+    s4LeftPossibilities = results[2]; // should be empty
+
+    // squareFour results:
     return [s4Left[0], s4Center[0],s4Right[0],
             s4Left[1], s4Center[1], s4Right[1],
             s4Left[2], s4Center[2], s4Right[2]];
